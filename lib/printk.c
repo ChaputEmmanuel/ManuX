416819139979f02dbed0f3ba85f328083896b0f6
Emmanuel Chaput <manu@hobbit.manu-chaput.net>
2023-12-04 09:05:46 +0100

    vsnprintk added

--- lib/printk.c
+++ lib/printk.c
@@ -23,6 +23,14 @@
 
 /**
  * @brief : Écriture formattée dans une chaîne de caractères
+ * @param  str : chaîne (allouée) dans laquelle l'écriture est faite
+ * @param l : capacité de la chaîne str
+ * @param format : le format d'affichage
+ * @argList : les arguments de l'affichage
+ *
+ * C'est cette  fonction qui fait tout le travail. Elle écrit dans une
+ * chaîne de caractères (allouée par l'appelant) en prenant garde de
+ * ne pas dépasser, quitte à ne pas tout écrire.
  */
 int vsnprintk(char * str, const size_t l, char * format, va_list argList)
 {
@@ -127,111 +135,20 @@ affent :          switch (prefixe) {
 
 /**
  * @brief : Écriture formattée dans une chaîne de caractères
+ *
+ * C'est cette fonction qui doit être utilisée pour écrire dans une
+ * chaîne de caractères
  */
-int sprintk(char * str, char * format, ...)
+int snprintk(char * str, size_t l, char * format, ...)
 {
    va_list   argList;
-   int       indice = 0;
-   long long int       n;   // valeur associée à un %[l]d
-   char      nombre[10];    // chaîne du nombre
-   char    * s;             // valeur associée à un %s
-   char      c;             // Affichage d'un caractère
-   int       in;            // indice pour les boucles internes
-   int       nbChiffres;    // pour les %[n]d
-   int       base;          // de l'affichage entier
-   int       prefixe;       // 0 int, 1 long int, 2 long long int
-   
+   int       result;
+
    va_start(argList, format);
-
-   while (*format) {
-      switch (*format) {
-         case '%' :
-            format++;
-            // Lecture de la taille
-            nbChiffres = 0;
-            while ((*format <= '9') && (*format >= '0')) {
-               nbChiffres = nbChiffres * 10 + *format - '0';
-               format++;
-	    }
-
-	    // Pas forcément bien là, mais j'ai pas mieux dans l'immédiat
-	    prefixe = 0;
-	    if (*format == 'l') {
-               format++;
-	       if (*format == 'l') {
-                  format++;
- 		  prefixe = 2;
-	       } else {
- 		  prefixe = 1;
-	       }
-	    }
-            switch (*format) {
-	       case 'o' :
-                  base = 8;
-                  goto affent;
-	       case 'x' :
-                  base = 16;
-                  goto affent;
-	       case 'd' :
-                  base = 10;
-affent :          switch (prefixe) {
-                     case 2 :
-		        n = va_arg(argList, long long int);
-		     break;
-                     case 1 :
-		        n = va_arg(argList, long int);
-		     break;
-                     default :
-		        n = va_arg(argList, int);
-                     break;
-		  }
-                  if (n < 0) {
-		     n = -n;
-		     str[indice++] = '-';
-		  }
-                  in = 0;
-                  do {
-                     nombre[in++] = chiffre[n%base];
-                     n = n/base;
-		  } while (n != 0);
-                  while (nbChiffres > in) {
-                     nbChiffres--;
-                     str[indice++] = ' ';
-		  }
-                  do {
-                     str[indice++] = nombre[--in];
-                  } while (in);
-               break; 
-               case 's' :
-                  s = va_arg(argList, char *);
-                  in = 0;
-                  while (s[in]) {
-                     str[indice++] = s[in++];
-		  }
-               break;
-               case 'c' :
-                  c = va_arg(argList, char );
-                  in = 0;
-                  str[indice++] = c;
-               break;
-               default :
-               break; 
-	    }
-         break;
-	 default :
-            str[indice++] = *format;
-         break;
-      }
-      format++;
-   }
-
-   assert(indice < MAX_PRINTK_LENGTH);
-   
-   str[indice] = 0;
-
+   result = vsnprintk(str, l, format, argList);
    va_end(argList);
 
-   return indice;
+   return result;
 }
 
 void printk(char * format, ...)
