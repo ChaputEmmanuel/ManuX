da534cbbdc86105bcdff696d5f0134720a7ba755
Emmanuel Chaput <emmanuel.chaput@toulouse-inp.fr>
2024-12-12 15:11:27 +0100

    asm volatile in/out

---
+++ multiconf/virtio-net.h
@@ -0,0 +1,37 @@
+/**
+ * @file multiconf/virtio-net.h
+ * @brief Utilisation d'une interface réseau de type virtio
+ *
+ *                                                     (C) Manu Chaput 2000-2024
+ */
+
+#ifdef MANUX_FICHIER_CONFIG
+#   error "Cette chose ne devrait pas se produire !"
+#   include MANUX_FICHIER_CONFIG
+#else
+#ifndef MANUX_CONFIG
+#define MANUX_CONFIG
+
+#define MANUX_FICHIER_MAIN main-virtio-net
+
+#include <config/base.h>
+#include <config/bootloader.h>
+#include <config/plan-memoire.h>
+#include <config/gestion-memoire.h>
+#include <config/kmalloc.h>   // Parce que dans tache, on hérite des fichiers
+#include <config/taches.h>
+#include <config/synchronisation.h>
+#undef MANUX_EXCLUSION_MUTUELLE_AUDIT
+#undef MANUX_CONDITION_AUDIT
+#include <config/console.h>
+#include <config/printk.h>
+#include <config/pc-i386.h>
+#include <config/pci.h>
+#include <config/virtio.h>
+#undef MANUX_VIRTIO_CONSOLE
+#include <config/reseau.h>
+
+#include <config/verifications.h>
+
+#endif  // MANUX_CONFIG
+#endif  // MANUX_FICHIER_CONFIG
---
+++ noyau/main-virtio-net.c
@@ -0,0 +1,47 @@
+/**
+ * @file : noyau/main-net.c
+ * @brief : Un exemple de début de noyau utilisant les éléments de base réseau.
+ *                                                                            
+ *                                                     (C) Manu Chaput 2000-2024
+ **/
+#include <manux/config.h>
+#include <manux/debug.h>
+#include <manux/bootloader.h>
+#include <manux/errno.h>
+#include <manux/console.h>
+#include <manux/memoire.h>
+#include <manux/interruptions.h>
+#include <manux/pci.h>
+#include <manux/virtio-net.h>
+
+void startManuX()
+{
+   // Initialisation de la console noyau
+   consoleInitialisation();
+
+   bootloaderInitialiser();
+
+   /* Initialisation de la gestion mémoire */
+   initialiserMemoire(infoSysteme.memoireDeBase,
+		      infoSysteme.memoireEtendue);
+   
+   /* Initilisation des descripteurs de segments */
+   initialiserGDT();
+
+   /* Initialisation de la table des interruptions */
+   initialiserIDT();   
+
+   /* Initialisation du bus PCI */
+   printk_debug(DBG_KERNEL_START, "Initialisation du bus PCI ...\n");
+   PCIEnumerationDesEquipements();
+   printk_debug(DBG_KERNEL_START, "Bus PCI initialise...\n");
+
+   printk_debug(DBG_KERNEL_START, "Initialisation de virtio net ...\n");
+   if (virtioNetInit() != ESUCCES) {
+      printk_debug(DBG_KERNEL_START, "Impossible d'initialiser periph reseau ...\n");
+   }
+
+   
+}   /* startManuX */
+
+
